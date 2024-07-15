fc363662a26eb3ac5156e42b4b1d54f97a608a05
Fathanrachel <racheltambunan10@gmail.com>
2024-07-15 10:35:57 +0700

    new

---
+++ script.js
@@ -0,0 +1,61 @@
+//home
+document.getElementById("home").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("homesection").scrollIntoView({ behavior: 'smooth' });
+});
+
+//footer-home
+document.getElementById("footer-home").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("homesection").scrollIntoView({ behavior: 'smooth' });
+});
+
+// skill
+document.getElementById("skill").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("skillsection").scrollIntoView({ behavior: 'smooth' });
+});
+
+// footer-skill
+document.getElementById("footer-skill").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("skillsection").scrollIntoView({ behavior: 'smooth' });
+});
+
+// about me
+document.getElementById("about").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("aboutmesection").scrollIntoView({ behavior: 'smooth' });
+});
+
+//footer about me
+document.getElementById("footer-about").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("aboutmesection").scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// contact
+document.getElementById("contact").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("contactsection").scrollIntoView({ behavior: 'smooth' });
+});
+
+// footer contact
+document.getElementById("footer-contact").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("contactsection").scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// portofolio
+document.getElementById("portofolio").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("portofoliosection").scrollIntoView({ behavior: 'smooth' });
+});
+
+// footer portofolio
+document.getElementById("footer-portofolio").addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah tindakan default dari link
+    document.getElementById("portofoliosection").scrollIntoView({ behavior: 'smooth' });
+});
